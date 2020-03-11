98712fa1f8ca638d8c6d888d5e85f1b80c2bfd4e
Ezekiel Eromosei <ezekiel.eromosei@gmail.com>
2020-03-11 17:47:45 +0100

    unbounded height bug fixed. closes #3

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -14,42 +14,45 @@ class TransactionList extends StatelessWidget {
     //    NumberFormat f = NumberFormat.currency(locale: "en_US", symbol: "€");
     NumberFormat f = NumberFormat.currency(locale: "en_US", symbol: "\$");
 
-    return ListView(
-      children: transactions
-          .map((tx) => Card(
-                  child: Row(
-                children: <Widget>[
-                  Container(
-                    margin: EdgeInsets.only(left: 10, right: 15),
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Colors.purple,
-                        width: 2.0,
+    return Container(
+      height: 400,
+      child: ListView(
+        children: transactions
+            .map((tx) => Card(
+                    child: Row(
+                  children: <Widget>[
+                    Container(
+                      margin: EdgeInsets.only(left: 10, right: 15),
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.purple,
+                          width: 2.0,
+                        ),
+                      ),
+                      padding: EdgeInsets.all(10.0),
+                      child: Text(
+                        '${f.currencySymbol}${tx.amount}',
+                        style: kTextStyler,
                       ),
                     ),
-                    padding: EdgeInsets.all(10.0),
-                    child: Text(
-                      '${f.currencySymbol}${tx.amount}',
-                      style: kTextStyler,
-                    ),
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        '${tx.title}',
-                        style: kTextStyler.copyWith(
-                            fontSize: 16.0, color: Colors.black),
-                      ),
-                      Text(
-                          '${DateFormat.yMMMEd().format(tx.date)}', // you can use pattern too
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          '${tx.title}',
                           style: kTextStyler.copyWith(
-                              color: Colors.grey, fontSize: 15)),
-                    ],
-                  )
-                ],
-              )))
-          .toList(),
+                              fontSize: 16.0, color: Colors.black),
+                        ),
+                        Text(
+                            '${DateFormat.yMMMEd().format(tx.date)}', // you can use pattern too
+                            style: kTextStyler.copyWith(
+                                color: Colors.grey, fontSize: 15)),
+                      ],
+                    )
+                  ],
+                )))
+            .toList(),
+      ),
     );
   }
 }
