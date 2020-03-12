15ceabe4232bedda55d57127e05f595d532866bc
Ezekiel Eromosei <ezekiel.eromosei@gmail.com>
2020-03-12 20:08:27 +0100

    Adding default custom AppBarTheme.  closes #15

--- lib/main.dart
+++ lib/main.dart
@@ -15,10 +15,15 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Personal Expenses',
       theme: ThemeData(
-        primarySwatch: Colors.purple,
-        accentColor: Colors.amber,
-        fontFamily: 'Quicksand',
-      ),
+          primarySwatch: Colors.purple,
+          accentColor: Colors.amber,
+          fontFamily: 'Quicksand',
+          appBarTheme: AppBarTheme(
+              textTheme: ThemeData.light().textTheme.copyWith(
+                      title: TextStyle(
+                    fontFamily: "OpenSans",
+                    fontSize: 20,
+                  )))),
       home: MyHomePage(),
     );
   }
