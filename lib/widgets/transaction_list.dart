38d1d508aebd9159f7fef7638da550172b0ee3b2
Ezekiel Eromosei <ezekiel.eromosei@gmail.com>
2020-03-17 09:10:27 +0100

    ADDED IMAGE AND FITTED IMAGE IN A SMALLER SPACE.  closes #17, closes #18

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -16,45 +16,63 @@ class TransactionList extends StatelessWidget {
 
     return Container(
       height: 400,
-      child: ListView.builder(
-        itemBuilder: (ctx, index) {
-          return Card(
-            child: Row(
+      child: transactions.isEmpty
+          ? Column(
               children: <Widget>[
-                Container(
-                  margin: EdgeInsets.only(left: 10, right: 15),
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                      color: Theme.of(context).primaryColor,
-                      width: 2.0,
-                    ),
-                  ),
-                  padding: EdgeInsets.all(10.0),
-                  child: Text(
-                    '${f.currencySymbol}${transactions[index].amount.toStringAsFixed(2)}',
-                    style: kTextStyler.copyWith(
-                        color: Theme.of(context).primaryColor),
-                  ),
+                Text(
+                  'No transactions added yet',
+                  style: Theme.of(context).textTheme.title,
                 ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text(
-                      '${transactions[index].title}',
-                      style: Theme.of(context).textTheme.title,
-                    ),
-                    Text(
-                        '${DateFormat.yMMMEd().format(transactions[index].date)}', // you can use pattern too
-                        style: kTextStyler.copyWith(
-                            color: Colors.grey, fontSize: 15)),
-                  ],
-                )
+                SizedBox(
+                  height: 20.0,
+                ),
+                Container(
+                    height: 200,
+                    child: Image.asset(
+                      'assets/images/waiting.png',
+                      fit: BoxFit.cover,
+                    ))
               ],
+            )
+          : ListView.builder(
+              itemBuilder: (ctx, index) {
+                return Card(
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                        margin: EdgeInsets.only(left: 10, right: 15),
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Theme.of(context).primaryColor,
+                            width: 2.0,
+                          ),
+                        ),
+                        padding: EdgeInsets.all(10.0),
+                        child: Text(
+                          '${f.currencySymbol}${transactions[index].amount.toStringAsFixed(2)}',
+                          style: kTextStyler.copyWith(
+                              color: Theme.of(context).primaryColor),
+                        ),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            '${transactions[index].title}',
+                            style: Theme.of(context).textTheme.title,
+                          ),
+                          Text(
+                              '${DateFormat.yMMMEd().format(transactions[index].date)}', // you can use pattern too
+                              style: kTextStyler.copyWith(
+                                  color: Colors.grey, fontSize: 15)),
+                        ],
+                      )
+                    ],
+                  ),
+                );
+              },
+              itemCount: transactions.length,
             ),
-          );
-        },
-        itemCount: transactions.length,
-      ),
     );
   }
 }
