848d867ea23e1f47e89f5fc2e2c4fb252c1807e4
Ezekiel Eromosei <ezekiel.eromosei@gmail.com>
2020-03-19 17:50:36 +0100

    removing scrollability by removing default padding. closes #34

--- lib/main.dart
+++ lib/main.dart
@@ -118,13 +118,15 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           children: <Widget>[
             Container(
-                height: ((MediaQuery.of(context).size.height) -
-                        appBar.preferredSize.height) *
+                height: (MediaQuery.of(context).size.height -
+                        appBar.preferredSize.height -
+                        MediaQuery.of(context).padding.top) *
                     0.4,
                 child: Chart(recentTransactions: _recentTransactions)),
             Container(
-              height: ((MediaQuery.of(context).size.height) -
-                      appBar.preferredSize.height) *
+              height: (MediaQuery.of(context).size.height -
+                      appBar.preferredSize.height -
+                      MediaQuery.of(context).padding.top) *
                   0.6,
               child: TransactionList(
                   transactions: _userTransactions,
